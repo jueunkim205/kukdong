504a371b7eebb3f75c84ec33fc3ed399d23d53d9
jueunkim205~ <ga473625@gmail.com>
2025-04-15 11:55:11 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,102 @@
+
+//---------------------------- 메인메뉴 ----------------------------
 let nav = document.querySelector("nav");
 let mainMenu = document.querySelector(".mainMenu");
 let mainMenuLi = document.querySelectorAll(".mainMenu li");
 let subMenu = document.querySelector(".subMenu");
 let subMenuItem = document.querySelectorAll(".subMenuItem");
+
+function addClass(k) {
+  // 순서에 해당하는 sub메뉴 클래스 추가
+  subMenuItem.forEach(function (item, index) {
+    mainMenuLi[k].querySelector("a").classList.add("on");
+    subMenuItem[k].classList.add("on");
+  });
+}
+
+// 순서에 해당하는 sub메뉴 클래스 삭제
+function rmClass() {
+  subMenuItem.forEach(function (v, k) {
+    mainMenuLi[k].querySelector("a").classList.remove("on");
+    subMenuItem[k].classList.remove("on");
+  });
+}
+
+// 메인메뉴 리스트에 마우스 올릴때마다
+mainMenuLi.forEach(function (v, k) {
+  v.onmouseenter = function () {
+    rmClass();
+    addClass(k);
+    mainMenuLi[k].querySelector("a").classList.add("on");
+  };
+});
+
+// mainMenu에서 mouseleave할때마다 subMenu도 사라져야 하지만
+// subMenu에 mouseenter할때는 subMenu가 사라지면 안됨
+mainMenu.addEventListener("mouseenter", function () {
+  nav.classList.add("on");
+});
+
+mainMenu.addEventListener("mouseleave", function () {
+  nav.classList.remove("on");
+
+  subMenu.addEventListener("mouseenter", function () {
+    nav.classList.add("on");
+  });
+  subMenu.addEventListener("mouseleave", function () {
+    nav.classList.remove("on");
+  });
+});
+
+
+//---------------------------- top버튼 ----------------------------
 let topBtn = document.getElementById("topbtn");
+
+topBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+window.addEventListener("scroll", () => {
+  //스크롤을 할 때마다 로그로 현재 스크롤의 위치가 찍혀나온다.
+  // console.log(window.scrollX, window.scrollY);
+  let scY = window.scrollY;
+  if (scY <= 50) {
+    topBtn.style.transform = "translateX(100px)";
+    topBtn.style.transition = ".5s";
+  } else {
+    topBtn.style.transform = "translateX(0)";
+  }
+});
+
+
+//---------------------------- side버튼의 전체메뉴 띄우기 ----------------------------
+let openBtn = document.getElementById("open");
+let closeBtn = document.getElementById("close");
+let allMenu = document.getElementById("allMenu");
+let darkbg = document.querySelector(".darkbg");
+
+openBtn.addEventListener("click", function () {
+  allMenu.classList.add("on");
+  darkbg.classList.add("on");
+});
+
+closeBtn.addEventListener("click", function () {
+  allMenu.classList.remove("on");
+  darkbg.classList.remove("on");
+});
+
+darkbg.addEventListener("click", function () {
+  allMenu.classList.remove("on");
+  darkbg.classList.remove("on");
+});
+
+
+//---------------------------- 메인슬라이드 ----------------------------
 let slide = document.querySelectorAll("#visual .swiper-slide");
 let txt = document.querySelectorAll("#visual .txt>div");
-
 document.querySelector(".start").innerHTML = "1";
 document.querySelector(".end").innerHTML = slide.length;
+
 
 function myfuc() {
   slide.forEach(function (v, k) {
@@ -61,92 +149,7 @@ document.querySelector(".play").onclick = function () {
 };
 
 
-
-// 메인메뉴
-
-function addClass(k) {
-  // 순서에 해당하는 sub메뉴 클래스 추가
-  subMenuItem.forEach(function (item, index) {
-    mainMenuLi[k].querySelector("a").classList.add("on");
-    subMenuItem[k].classList.add("on");
-  });
-}
-
-// 순서에 해당하는 sub메뉴 클래스 삭제
-function rmClass() {
-  subMenuItem.forEach(function (v, k) {
-    mainMenuLi[k].querySelector("a").classList.remove("on");
-    subMenuItem[k].classList.remove("on");
-  });
-}
-
-// nav.classList.remove('on');
-
-// 메인메뉴 리스트에 마우스 올릴때마다
-mainMenuLi.forEach(function (v, k) {
-  v.onmouseenter = function () {
-    rmClass();
-    addClass(k);
-    mainMenuLi[k].querySelector("a").classList.add("on");
-  };
-});
-
-// mainMenu에서 mouseleave할때마다 subMenu도 사라져야 하지만
-// subMenu에 mouseenter할때는 subMenu가 사라지면 안됨
-mainMenu.addEventListener("mouseenter", function () {
-  nav.classList.add("on");
-});
-
-mainMenu.addEventListener("mouseleave", function () {
-  nav.classList.remove("on");
-
-  subMenu.addEventListener("mouseenter", function () {
-    nav.classList.add("on");
-  });
-  subMenu.addEventListener("mouseleave", function () {
-    nav.classList.remove("on");
-  });
-});
-
-// top버튼
-topBtn.onclick = function () {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
-
-window.addEventListener("scroll", () => {
-  //스크롤을 할 때마다 로그로 현재 스크롤의 위치가 찍혀나온다.
-  // console.log(window.scrollX, window.scrollY);
-  let scY = window.scrollY;
-  if (scY <= 50) {
-    topBtn.style.transform = "translateX(100px)";
-    topBtn.style.transition = ".5s";
-  } else {
-    topBtn.style.transform = "translateX(0)";
-  }
-});
-
-// 전체메뉴
-let openBtn = document.getElementById("open");
-let closeBtn = document.getElementById("close");
-let allMenu = document.getElementById("allMenu");
-let darkbg = document.querySelector(".darkbg");
-
-openBtn.addEventListener("click", function () {
-  allMenu.classList.add("on");
-  darkbg.classList.add("on");
-});
-
-closeBtn.addEventListener("click", function () {
-  allMenu.classList.remove("on");
-  darkbg.classList.remove("on");
-});
-
-darkbg.addEventListener("click", function () {
-  allMenu.classList.remove("on");
-  darkbg.classList.remove("on");
-});
-
-// 포트폴리오
+//---------------------------- 포트폴리오 슬라이드 부분 ----------------------------
 var swiper2 = new Swiper(".mySwiper2", {
   effect: "fade",
   loop: true,
